61728a5071c400e40eaca21439bb877eef5e40b1
Galin Yanakov <galin@ivel.in>
2025-10-31 12:28:04 +0200

    Init

---
+++ src/main/java/io/ethertale/parmentierpenshop/ParmentierPenShopApplication.java
@@ -0,0 +1,13 @@
+package io.ethertale.parmentierpenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParmentierPenShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParmentierPenShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ethertale/parmentierpenshop/ParmentierPenShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ethertale.parmentierpenshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParmentierPenShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
